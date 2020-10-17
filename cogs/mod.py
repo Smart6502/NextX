18dddf87277fb7832d1d136188b0f9662d4b631c
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2020-10-17 09:34:36 +0530

    Updated userinfo command to show more stuff

--- cogs/mod.py
+++ cogs/mod.py
@@ -89,27 +89,25 @@ class Moderation(Cog):
 
             await ctx.send(f'{user.mention} Unbanned!.')
 
-    @command(aliases=['user-info','memberinfo'])
-    async def userinfo(self,ctx,member: discord.Member):
-        if member.guild_permissions.administrator:
-            admin = "Yes"
-        else:
-            admin = "No"
-        if member.bot:
-            bot = "Yes"
-        else:
-            bot = "No"
-        created = member.created_at
-        joined = member.joined_at
-        embed=discord.Embed(title=f"{member}")
-        embed.set_thumbnail(url=f"{member.avatar_url}")
-        embed.add_field(name="Account created", value=f"{created.strftime('%Y-%m-%d')}", inline=True)
-        embed.add_field(name="Nickname", value=f"{member.nick}", inline=True)
-        embed.add_field(name="ID", value=f"{member.id}", inline=True)
-        embed.add_field(name="Joined at", value=f'{joined.strftime("%Y-%m-%d")}', inline=True)
-        embed.add_field(name="Is Admin",value=f'{admin}', inline=True)
-        embed.add_field(name="Is Bot",value=f'{bot}', inline=True)
-        embed.add_field(name=f'Roles', value=f'{len(member.roles)}')
+    @command(aliases=["whois"])
+    async def userinfo(self, ctx, member: discord.Member = None):
+        if not member:
+            member = ctx.message.author
+        roles = [role for role in member.roles]
+        embed = discord.Embed(colour=discord.Colour.purple(), timestamp=ctx.message.created_at,
+                            title=f"User Info - {member}")
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.set_footer(text=f"Requested by {ctx.author}")
+
+        embed.add_field(name="ID:", value=member.id)
+        embed.add_field(name="Display Name:", value=member.display_name)
+
+        embed.add_field(name="Created Account On:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+        embed.add_field(name="Joined Server On:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+
+        embed.add_field(name="Roles:", value="".join([role.mention for role in roles]))
+        embed.add_field(name="Highest Role:", value=member.top_role.mention)
+        print(member.top_role.mention)
         await ctx.send(embed=embed)
 
 
